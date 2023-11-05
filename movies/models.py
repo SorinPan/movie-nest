88209049a00454b0a54769a3f09b12fea2a9a278
Sorin Panait <sorin.panait0@gmail.com>
2023-11-05 17:36:13 +0000

    Create movies model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    """
+    Model for movie
+    """
+    movie_id = CharField(max_length=25, unique=True)
+    title = models.CharField(max_length=255)
+    overview = models.TextField()
+    release_date = models.DateField()
+    certification = models.CharField(max_length=10)
+    runtime = models.IntegerField()
+    director = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
