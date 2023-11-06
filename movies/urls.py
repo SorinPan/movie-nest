af314c6e3a1e3ab25affae29a647b8182babd74a
Sorin Panait <sorin.panait0@gmail.com>
2023-11-06 10:35:12 +0000

    Create structure template for movie_details and view

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('top-rated/', views.movie_categories, {'category': 'top_rated'}, name='top_rated_movies'),
     path('trending/', views.movie_categories, {'category': 'trending'}, name='trending_movies'),
+    path("movie_details/<int:movie_id>", views.movie_details, name="movie_details"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -6,7 +6,7 @@ API_KEY = os.environ.get("API_KEY")
 
 def movie_categories(request, category, page_number=1):
     """
-    Request movie data from TMDB api based on category
+    Request movie info from TMDB api based on category
     """
     category_mapping = {
         "top_rated": {
@@ -40,3 +40,28 @@ def movie_categories(request, category, page_number=1):
         return render(request, template_name, context)
     else:
         return ("Something went wrong!")
+
+def movie_details(request, movie_id):
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US"
+        response = requests.get(url)
+        response.raise_for_status()
+        movie_info = response.json()
+
+        
+        if "original_title" in movie_info and "overview" in movie_info:
+            backdrop = movie_info.get("backdrop_path", "")
+            hero = f"https://image.tmdb.org/t/p/w1280/{backdrop}"
+
+            context = {
+                "movie_info": movie_info,
+                "hero": hero,
+            }
+
+            return render(request, "movies/movie_details.html", context)
+        else:
+            return render(request, "error.html", {"message": "Invalid movie data received from the API."})
+
+    except requests.exceptions.RequestException as e:
+        # Handle request-related errors, such as connection issues or timeouts
+        return render(request, "error.html", {"message": f"API request failed: {str(e)}"})
